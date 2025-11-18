4e931cfd4df6f08a3dd01f737fa20342c0a02e44
Gruandal <127666769+Gruandal@users.noreply.github.com>
2025-11-18 23:13:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # app.py
 import os 
-import sys # 故意留一個未使用的 import，會被 Flake8 警告
+import sys # F401: 'sys' imported but unused
 
-def calculate_sum( a,b): # 故意在參數周圍加入多餘空格，會被 Flake8 警告
-    print("Hello DevSecOps World!")
+def calculate_sum(a,b ): # E202: whitespace before ')' 
+    
+    # W293: blank line contains whitespace
+    
+    if True: print("This is a bad style.") # E701: multiple statements on one line 
+    
     return a + b
 
 result = calculate_sum(10, 5)
